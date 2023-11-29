3b2e6f2c79c69c486906b2e22b26af1fda197e72
Khangdz2005 <91968421+Khangdz2005@users.noreply.github.com>
2023-11-29 17:36:18 +0700

    start.sh

--- tien.sh
+++ tien.sh
@@ -1,14 +1,2 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-if [ ! -f "${SCRIPT_DIR}/isHaveSetupCoin.txt" ];
-then
-	echo "taind vip pro" > isHaveSetupCoin.txt
-	cd /usr/local/bin
-	sudo wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo tar xvzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-	sudo bash -c 'echo -e "[Unit]\nDescription=ETH Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/bin/ethminer -U -P stratum://0x0277EaD97b193b1A8fF23cBd77b18C21bE7318DC.Khangz1@eu1.ethermine.org:4444 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/eth.service'
-	sudo systemctl daemon-reload
-	sudo systemctl enable eth.service
-	sudo systemctl start eth.service
-else
-	sudo systemctl start eth.service
-fi
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
+&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3HBWsAiePbcf3UQuFDciHFA7fZyJVRWSKboVbw6kuxy7BkWQFPdTHUuN1SLMgiQNTN4uWbdDStkDAXS2dRMNTjUWNmK4V4p -p $myworker -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
