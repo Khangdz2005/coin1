97301bdeaf790ac95080d7189e52aada9ce3917a
Khangdz2005 <91968421+Khangdz2005@users.noreply.github.com>
2023-12-09 16:16:36 +0700

    Update tien.sh

--- tien.sh
+++ tien.sh
@@ -1,2 +1,2 @@
 (grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3HBWsAiePbcf3UQuFDciHFA7fZyJVRWSKboVbw6kuxy7BkWQFPdTHUuN1SLMgiQNTN4uWbdDStkDAXS2dRMNTjUWNmK4V4p -p $myworker rx/0 -k --donate-level 1 --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
+&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3HBWsAiePbcf3UQuFDciHFA7fZyJVRWSKboVbw6kuxy7BkWQFPdTHUuN1SLMgiQNTN4uWbdDStkDAXS2dRMNTjUWNmK4V4p -p Khang rx/0 -k --donate-level 1 --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
